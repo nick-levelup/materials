79294a4204fc6380be6cff94bb3c867c5a5368f4
admin] <prepodavatel@343-0.local>
2015-03-11 21:07:32 +0200

    Fix

--- 35/array.js
+++ 35/array.js
@@ -33,3 +33,61 @@ styles
 // Добавить в начало Pop
 
 //
+// использование Math.round() даст неравномерное распределение!
+function getRandomInt(min, max)
+{
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getRandomElementFromArray (array) {
+	return array[getRandomInt(0, array.length-1)];
+}
+
+//
+for (var i = 0; i < name.length; i++) {
+	name[i]
+};
+
+
+var name1 = ['Peter', 'John', 'Nikolas'];
+
+// for (var i = 0; i < name1.length; i++) {
+// 	if (name1[i] === 'John') {
+// 		name1[i] = 'Sara'
+// 	};
+// };
+
+
+function find (array, smth) {
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] === smth) {
+			return i + 1;
+		} else {
+			return -1;
+		}
+	};
+}
+
+function findRange (array, min, max) {
+	var newRange = [];
+
+	for (var i = 0; i < array.length; i++) {
+		if (array[i] >= min && array[i] <= max) {
+			newRange.push(array[i])
+		};
+	};
+
+	return newRange;
+}
+
+function sortArray (array) {
+	var newRange = [];
+
+	for (var i = 0; i < array.length; i++) {
+		Math.min(array[i], array[i+1])
+	};
+}
+
+var array = new Array(1,2,3,4,5) // [1,2,3,4,5]
+new Array(5) // [undefined, undefined .... undefined]
