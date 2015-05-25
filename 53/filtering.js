76723d3b72e1f45b57838f8f82e49b05c397fabc
Nikita Stepanenko <nstepanenko@englishdom.com>
2015-05-25 18:09:50 +0300

    Fix

---
+++ 53/filtering.js
@@ -0,0 +1,42 @@
+/* =============== */
+.eq()
+/* Reduce the set of matched elements to the one at
+the specified index. */
+
+/* =============== */
+.filter()
+/* Reduce the set of matched elements to those that match
+the selector or pass the function’s test. */
+
+/* =============== */
+.first()
+/* Reduce the set of matched elements to the first in the set. */
+
+/* =============== */
+.has()
+/* Reduce the set of matched elements to those that have
+a descendant that matches the selector or DOM element. */
+
+/* =============== */
+.is()
+/* Check the current matched set of elements against
+a selector, element, or jQuery object and return true if
+at least one of these elements matches the given arguments. */
+
+/* =============== */
+.last()
+/* Reduce the set of matched elements to the final one in the set. */
+
+/* =============== */
+.map()
+/* Pass each element in the current matched set through
+a function, producing a new jQuery object containing the return values. */
+
+/* =============== */
+.not()
+/* Remove elements from the set of matched elements. */
+
+/* =============== */
+.slice()
+/* Reduce the set of matched elements to a subset
+specified by a range of indices. */
---
+++ 53/tree-traversal.js
@@ -0,0 +1,95 @@
+/* ============= */
+.children();
+/*
+Get the children of each element in the set of matched elements,
+optionally filtered by a selector.
+*/
+
+
+/* ============= */
+.find();
+/*
+Get the descendants of each element in the current set of matched
+elements, filtered by a selector, jQuery object, or element.
+*/
+
+
+/* ============= */
+.parent();
+/*
+Get the parent of each element in the current set of matched
+elements, optionally filtered by a selector.
+*/
+
+
+/* ============= */
+.parents();
+/*
+Get the ancestors of each element in the current set of
+matched elements, optionally filtered by a selector.
+*/
+
+
+/* ============= */
+.parentsUntil();
+/*
+Get the ancestors of each element in the current set of
+matched elements, up to but not including the element matched
+by the selector, DOM node, or jQuery object.
+*/
+
+
+/* ============= */
+.offsetParent();
+/*
+Get the closest ancestor element that is positioned.
+*/
+
+
+/* ============= */
+.closest();
+/*
+For each element in the set, get the first element that matches
+the selector by testing the element itself and traversing up through
+its ancestors in the DOM tree.
+*/
+
+
+/* ============= */
+.next();
+/*
+Get the immediately following sibling of each element in the
+set of matched elements. If a selector is provided, it retrieves
+the next sibling only if it matches that selector.
+*/
+
+
+/* ============= */
+.nextAll();
+/*
+Get all following siblings of each element in the set of matched
+elements, optionally filtered by a selector.
+*/
+
+
+/* ============= */
+.nextUntil();
+/*
+Get all following siblings of each element up to but not including
+the element matched by the selector, DOM node, or jQuery object passed.
+*/
+
+/* ============= */
+.prev();
+/* ============= */
+.prevAll();
+/* ============= */
+.prevUntil();
+
+
+/* ============= */
+.siblings();
+/*
+Get the siblings of each element in the set of matched
+elements, optionally filtered by a selector.
+*/
