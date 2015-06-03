78a6132b7d0dc2769924de06fd25f3495689a40b
Nikita Stepanenko <nstepanenko@englishdom.com>
2015-06-03 18:00:46 +0300

    Fix

---
+++ 56/events.js
@@ -0,0 +1,143 @@
+.on()
+/* Attach an event handler function for one or more events to
+the selected elements. */
+
+.off()
+/* Remove an event handler. */
+
+.one()
+/* Attach a handler to an event for the elements. The handler is
+executed at most once per element per event type. */
+
+jQuery.proxy()
+/* Takes a function and returns a new one that will always have a
+particular context. */
+
+.trigger()
+/* Execute all handlers and behaviors attached to the matched
+elements for the given event type. */
+
+.triggerHandler()
+/* Execute all handlers attached to an element for an event. */
+
+.bind()
+/* Attach a handler to an event for the elements. */
+
+.unbind()
+/* Remove a previously-attached event handler from the elements. */
+
+.delegate()
+/* Attach a handler to one or more events for all elements that match
+the selector, now or in the future, based on a specific set of root
+elements. */
+
+.undelegate()
+/* Remove a handler from the event for all elements which match the
+current selector, based upon a specific set of root elements. */
+
+/*
+Browser Events
+.error()
+Bind an event handler to the “error” JavaScript event.
+
+.resize()
+Bind an event handler to the “resize” JavaScript event, or trigger
+that event on an element.
+
+.scroll()
+Bind an event handler to the “scroll” JavaScript event, or trigger
+that event on an element.
+
+Form Events
+.blur()
+Bind an event handler to the “blur” JavaScript event, or trigger that
+event on an element.
+
+.change()
+Bind an event handler to the “change” JavaScript event, or trigger that
+event on an element.
+
+.focus()
+Bind an event handler to the “focus” JavaScript event, or trigger that
+event on an element.
+
+.focusin()
+Bind an event handler to the “focusin” event.
+
+.focusout()
+Bind an event handler to the “focusout” JavaScript event.
+
+.select()
+Bind an event handler to the “select” JavaScript event, or trigger
+that event on an element.
+
+.submit()
+Bind an event handler to the “submit” JavaScript event, or trigger that
+event on an element.
+
+Documents event
+
+.load()
+Bind an event handler to the “load” JavaScript event.
+
+.ready()
+Specify a function to execute when the DOM is fully loaded.
+
+Keyboard events
+.keydown()
+Bind an event handler to the “keydown” JavaScript event, or trigger
+that event on an element.
+
+.keypress()
+Bind an event handler to the “keypress” JavaScript event, or
+trigger that event on an element.
+
+.keyup()
+Bind an event handler to the “keyup” JavaScript event, or trigger
+that event on an element.
+
+Mouse events
+
+.click()
+Bind an event handler to the “click” JavaScript event, or
+trigger that event on an element.
+
+.dblclick()
+Bind an event handler to the “dblclick” JavaScript event, or
+trigger that event on an element.
+
+.hover()
+Bind one or two handlers to the matched elements, to be executed
+when the mouse pointer enters and leaves the elements.
+
+.mousedown()
+Bind an event handler to the “mousedown” JavaScript event, or
+trigger that event on an element.
+
+.mouseenter()
+Bind an event handler to be fired when the mouse enters an element,
+or trigger that handler on an element.
+
+.mouseleave()
+Bind an event handler to be fired when the mouse leaves an element,
+or trigger that handler on an element.
+
+.mousemove()
+Bind an event handler to the “mousemove” JavaScript event,
+or trigger that event on an element.
+
+.mouseout()
+Bind an event handler to the “mouseout” JavaScript event,
+or trigger that event on an element.
+
+.mouseover()
+Bind an event handler to the “mouseover” JavaScript event,
+or trigger that event on an element.
+
+.mouseup()
+Bind an event handler to the “mouseup” JavaScript event,
+or trigger that event on an element.
+
+
+
+*/
