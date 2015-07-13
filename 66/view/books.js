58ba64cf183b67650521e48f4c7297884a86ffb6
admin] <prepodavatel@343-0.local>
2015-07-13 20:50:55 +0300

    Fix

--- 66/view/books.js
+++ 66/view/books.js
@@ -10,12 +10,16 @@ define([
 		booksTpl
 	) {
 
+
+
 	function renderBooks (data) {
 		$('.js-books').html(Handlebars.compile(booksTpl)(data));
 	}
 
 	function bind () {
 		$('.js-books').on('click', '.js-del', deleteBookEvent);
+		$('.js-form').on('submit', createBookEvent);
+		$('.js-books').on('click', '.js-save', saveBookEvent);
 	}
 
 	function deleteBookEvent () {
@@ -32,45 +36,36 @@ define([
 		})
 	}
 
+	function createBookEvent (event) {
+		var me = this;
+
+	    event.preventDefault();
+
+		require(['collections/books'], function (getBooks) {
+		    var $form = $(me),
+		        data = {};
+
+		    _.each($form.serializeArray(), function (item) {
+		      data[item.name] = item.value;
+		    });
+
+		    getBooks().add(data).save();	
+		})
+	}
+
+	function saveBookEvent () {
+		var me = this;
+
+		require(['collections/books'], function (getBooks) {
+		    var book = getBooks().get($(me).closest('li').data('id'))
+
+		    book.set('title', $(me).prev().val());
+
+		    book.save();
+		})		
+	}
+
 	bind();
-
-
-  // // DELETE BOOK
-  // $('.js-books').on('click', '.js-del', function () {
-  //   var book = books.get($(this).closest('li').data('id'))
-
-  //   book.destroy({
-  //     // success: function () {
-  //     // },
-  //     error: function () {
-  //       loadData(books.toJSON())
-  //     }
-  //   });
-  // });
-
-
-  // // ADD BOOK
-  // $('.js-form').on('submit', function (event) {
-  //   event.preventDefault();
-
-  //   var $form = $(this),
-  //       data = {};
-
-  //   _.each($form.serializeArray(), function (item) {
-  //     data[item.name] = item.value;
-  //   });
-
-  //   books.add(data).save();
-  // });
-
-  // // editing
-  // $('.js-books').on('click', '.js-save', function () {
-  //   var book = books.get($(this).closest('li').data('id'))
-
-  //   book.set('title', $(this).prev().val());
-
-  //   book.save();
-  // });
 
 
 	return {
