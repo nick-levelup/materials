ce0149d7941e919a3e46cb5b2c046e580e330ea4
nick <nstepanenko@englishdom.com>
2015-08-19 17:56:25 +0300

    Fix

---
+++ 78/Class.js
@@ -0,0 +1,108 @@
+/* ===== */
+
+class Название [extends Родитель]  {
+  constructor
+  методы
+}
+
+/* ===== */
+
+'use strict';
+
+class User {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayHi() {
+    alert(this.name);
+  }
+
+}
+
+let user = new User("Вася");
+user.sayHi(); // Вася
+
+/* ===== */
+
+// animal
+// name
+// run, stop
+
+/* ===== */
+
+// Функция constructor запускается при создании new User,
+// остальные методы — записываются в User.prototype.
+
+/* ===== */
+
+// Перепешите в старую реализацию
+
+/* ===== */
+
+// User нельзя вызывать без new, будет ошибка.
+// Объявление класса с точки зрения области видимости ведёт себя как let.
+// В частности, оно видно только текущем в блоке и только в коде, который находится ниже объявления (Function Declaration видно и до объявления).
+
+// Метод sayHi является именно методом, то есть имеет доступ к super.
+// Все методы класса работают в режиме use strict, даже если он не указан.
+// Все методы класса не перечислимы. То есть в цикле for..in по объекту их не будет.
+
+/* ===== */
+
+// Class Expression
+
+/* ===== */
+
+'use strict';
+
+let User = class {
+  sayHi() { alert('Привет!'); }
+};
+
+new User().sayHi();
+
+/* ===== */
+
+'use strict';
+
+let SiteGuest = class User {
+  sayHi() { alert('Привет!'); }
+};
+
+new SiteGuest().sayHi(); // Привет
+new User(); // ошибка
+
+/* ===== */
+
+// Геттеры, сеттеры
+
+/* ===== */
+
+'use strict';
+
+class User {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  // геттер
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  // сеттер
+  set fullName(newValue) {
+    [this.firstName, this.lastName] = newValue.split(' ');
+  }
+
+};
+
+let user = new User("Вася", "Пупков");
+alert( user.fullName ); // Вася Пупков
+user.fullName = "Иван Петров";
+alert( user.fullName ); // Иван Петров
+
+/* ===== */
--- 78/es2015-object.js
+++ 78/es2015-object.js
@@ -96,3 +96,97 @@ Number
 
 Object.setPrototypeOf(obj, newProto)
 
+/* ==== */
+
+// Функция Object.assign получает список объектов и копирует в первый target свойства из остальных.
+
+/* ==== */
+
+Object.assign(target, src1, src2...)
+
+/* ==== */
+
+'use strict';
+
+let user = { name: "Вася" };
+let visitor = { isAdmin: false, visits: true };
+let admin = { isAdmin: true };
+
+Object.assign(user, visitor, admin);
+
+// user <- visitor <- admin
+alert( JSON.stringify(user) ); // ?
+
+/* ==== */
+
+'use strict';
+
+let user = { name: "Вася", isAdmin: false };
+
+Object.assign({}, user); // ?
+
+/* ==== */
+
+// Новая функция для проверки равенства значений.
+// Возвращает true, если значения value1 и value2 равны, иначе false.
+// этот алгоритм сравнения, который называется SameValue
+
+/* ==== */
+
+Object.is(value1, value2)
+
+/* ==== */
+
+// Сравнение +0 и -0
+alert( Object.is(+0, -0)); // false
+alert( +0 === -0 );        // true
+
+// Сравнение с NaN
+alert( Object.is(NaN, NaN) ); // true
+alert( NaN === NaN );         // false
+
+/* ==== */
+
+// Методы объекта
+// 1. Более короткий синтаксис объявления.
+
+/* ==== */
+
+'use strict';
+
+let name = "Вася";
+let user = {
+  name,
+  // вместо "sayHi: function() {" пишем "sayHi() {"
+  sayHi() {
+    alert(this.name);
+  }
+};
+
+user.sayHi(); // Вася
+
+/* ==== */
+
+// 2. super
+
+/* ==== */
+
+'use strict';
+
+let animal = {
+  walk() {
+    alert("I'm walking");
+  }
+};
+
+let rabbit = {
+  __proto__: animal,
+  walk() {
+    alert(super.walk); // walk() { … }
+    super.walk(); // I'm walking
+  }
+};
+
+rabbit.walk();
+
+/* ==== */
